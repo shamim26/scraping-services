965e09339121f35f41c4b9c28778c59e91c4eb8a
shamim26 <shamimreja101@gmail.com>
2025-06-25 12:33:58 +0600

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
